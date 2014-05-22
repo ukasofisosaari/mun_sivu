cec61a36b1b7a4943092221d50bb4b5b1c03d534
Saku Rautiainen <saku.rautiainen@intel.com>
2014-05-22 15:07:04 +0300

    Hilights menu entries

--- js/menu.js
+++ js/menu.js
@@ -1,27 +1,29 @@
 $(document).ready(function() {
     $(".right").load("me.html");
     $('#me').css("background-color","#0969A2");
+
+    var menu_div_id;
     $('.menu_div').click(function() {
         $('.menu_div').css("background-color","#64A8D1");
         $(this).css("background-color","#0969A2");
-        var id = $(this).attr('id');
-        if( id === "me" )
+        menu_div_id = $(this).attr('id');
+        if( menu_div_id === "me" )
         {
             $(".right").load("me.html");
         }
-        else if( id === "projects" )
+        else if( menu_div_id === "projects" )
         {
             $(".right").load("projects.html");
         }
-        else if( id === "resume" )
+        else if( menu_div_id === "resume" )
         {
             $(".right").load("resume.html");
         }
-        else if( id === "contact" )
+        else if( menu_div_id === "contact" )
         {
             $(".right").load("contact.html");
         }
-        else if( id === "server" )
+        else if( menu_div_id === "server" )
         {
             //var data;
             //$.get( "http://hakala.dy.fi/cgi-bin/hello.cgi", function( data ) {
@@ -30,4 +32,14 @@ $(document).ready(function() {
             $(".right").load("server.html");
         }
     });
+    $('.menu_div').mouseover(function() {
+        $(this).css("background-color","#0969A2");
+    });
+    $('.menu_div').mouseleave(function() {
+        if( menu_div_id != $(this).attr('id'))
+        {
+            $(this).css("background-color","#64A8D1");
+        }
+    });
+    
 });
