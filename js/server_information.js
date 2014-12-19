6a389f1d4b79e754078c05abb8d0f08b9b903465
Saku Rautiainen <saku.rautiainen@iki.fi>
2014-12-19 22:25:08 +0200

    Added separate file for server page scripts

---
+++ js/server_information.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+	$.getJSON('/server_uptime.json', function load_uptime(data){
+		var uptime = data.uptime;
+		var unix_time = data.unix_time;
+		$('#uptime').HTML("Uptime: " + uptime +" Date: " + unix_time)});
+};
