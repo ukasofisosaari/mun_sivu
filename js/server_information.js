b802a490ebfae1e66ecc5e3c52415a6fa841b1c6
Saku Rautiainen <srautiainen@elara.(none)>
2014-12-30 10:10:36 +0200

    small letters

--- js/server_information.js
+++ js/server_information.js
@@ -2,5 +2,5 @@ $(document).ready(function() {
 	$.getJSON('/server_uptime.json', function load_uptime(data){
 		var uptime = data.uptime;
 		var unix_time = data.unix_time;
-		$('#uptime').HTML("Uptime: " + uptime +" Date: " + unix_time)});
+		$('#uptime').html("Uptime: " + uptime +" Date: " + unix_time)});
 });
